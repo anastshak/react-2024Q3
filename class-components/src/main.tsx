0951d3b8664445d29a82272dc5d600767a009240
anastshak <nastyashakura@gmail.com>
2024-07-13 15:38:59 +0300

    feat: add base route and change the app's name

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+// import App from './App.tsx';
 import './index.css';
-import { ErrorBoundary } from './components/Error/Error-boundary/Error-boundary.tsx';
-import { ErrorPage } from './components/Error/Error-page/Error-page.tsx';
+// import { ErrorBoundary } from './components/Error/Error-boundary/Error-boundary.tsx';
+// import { ErrorPage } from './components/Error/Error-page/Error-page.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary fallback={<ErrorPage />}>
+    <RouterProvider router={router} />
+    {/* <ErrorBoundary fallback={<ErrorPage />}>
       <App />
-    </ErrorBoundary>
+    </ErrorBoundary> */}
   </React.StrictMode>
 );
