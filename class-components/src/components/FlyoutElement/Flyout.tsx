01aa9d8c09c727a95e0b68836b863d7934d31ebb
anastshak <nastyashakura@gmail.com>
2024-07-27 22:14:08 +0300

    feat: add download function

--- class-components/src/components/FlyoutElement/Flyout.tsx
+++ class-components/src/components/FlyoutElement/Flyout.tsx
@@ -3,27 +3,42 @@ import { useAppDispatch, useAppSelector } from '../../hooks/useReduxStore';
 import { clearSelectedCards } from '../../store/selectedCharactersSlice';
 import classnames from 'classnames';
 import { useTheme } from '../../context/useTheme';
+import { convertToCSV } from '../../utils/utils';
 
 import styles from './Flyout.module.css';
 
 export default function FlyoutElement(): JSX.Element {
   const { theme } = useTheme();
-
   const dispatch = useAppDispatch();
   const selectedCharacters = useAppSelector((state) => state.selected.selectedCharacters);
+
+  const numberOfElements: number = selectedCharacters.length;
 
   const handleUnselect = () => {
     dispatch(clearSelectedCards());
   };
 
+  const handleDownload = () => {
+    const csvContent = convertToCSV(selectedCharacters);
+    const csvData = new Blob([csvContent], { type: 'text/csv' });
+    const csvURL = URL.createObjectURL(csvData);
+    return csvURL;
+  };
+
+  const downloadCSV = handleDownload();
+
   return (
     <div className={classnames(styles.flyout, { [styles.dark]: theme === 'light' })}>
-      <p className={styles.info}> {selectedCharacters.length} cards are selected: </p>
+      <p className={styles.info}> {numberOfElements} cards are selected: </p>
       <p className={styles.control}>
-        <button type="button" className={styles.btnDownload}>
+        <a
+          href={downloadCSV}
+          download={`${numberOfElements}_cards.csv`}
+          className={classnames(styles.btn, styles.btnDownload)}
+        >
           Download
-        </button>
-        <button type="button" className={styles.btnUnselect} onClick={handleUnselect}>
+        </a>
+        <button type="button" className={classnames(styles.btn, styles.btnUnselect)} onClick={handleUnselect}>
           Unselect all
         </button>
       </p>
