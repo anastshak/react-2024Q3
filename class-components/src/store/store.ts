d2c25faad4f7b8c2302577d702cac1f45a5fe0b4
anastshak <nastyashakura@gmail.com>
2024-08-03 15:24:01 +0300

    feat: update store with next-redux-wrapper

--- class-components/src/store/store.ts
+++ class-components/src/store/store.ts
@@ -2,15 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import { swapiApi } from './swapiApi';
 import { charactersReducer } from './charactersSlice';
 import { selectedCharactersReducer } from './selectedCharactersSlice';
+import { createWrapper } from 'next-redux-wrapper';
 
-export const store = configureStore({
-  reducer: {
-    [swapiApi.reducerPath]: swapiApi.reducer,
-    characters: charactersReducer,
-    selected: selectedCharactersReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(swapiApi.middleware),
-});
+const makeStore = () =>
+  configureStore({
+    reducer: {
+      [swapiApi.reducerPath]: swapiApi.reducer,
+      characters: charactersReducer,
+      selected: selectedCharactersReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(swapiApi.middleware),
+  });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<ReturnType<typeof makeStore>['getState']>;
+export type AppDispatch = ReturnType<typeof makeStore>['dispatch'];
+
+export const wrapper = createWrapper(makeStore);
