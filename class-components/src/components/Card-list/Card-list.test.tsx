fe3dfddb2acac07cf12cfc433ac75bac0f722855
anastshak <nastyashakura@gmail.com>
2024-08-09 20:29:24 +0300

    feat:update some tests

--- class-components/src/components/Card-list/Card-list.test.tsx
+++ class-components/src/components/Card-list/Card-list.test.tsx
@@ -1,8 +1,17 @@
 import { screen } from '@testing-library/react';
-import { describe, test, expect, vi } from 'vitest';
+import { describe, test, expect, vi, beforeEach, Mock } from 'vitest';
 import CardList from './Card-list';
 import { Character } from '../../types/types';
 import { renderWithProviders } from '../../test/render-with-providers';
+import { useRouter } from 'next/navigation';
+import userEvent from '@testing-library/user-event';
+
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn(),
+  useSearchParams: vi.fn(() => ({
+    get: vi.fn(() => ''),
+  })),
+}));
 
 describe('CardList component', () => {
   const mockCharacters: Character[] = [
@@ -16,17 +25,34 @@ describe('CardList component', () => {
     { name: 'Darth Vader', gender: 'male', height: 202, birth_year: '41.9BBY', url: 'https://swapi.dev/api/people/2/' },
   ];
 
-  const mockHandleCardClick = vi.fn();
+  const pushMock = vi.fn();
+  const mockedUseRouter = useRouter as Mock;
+
+  beforeEach(() => {
+    pushMock.mockClear();
+    mockedUseRouter.mockReturnValue({
+      push: pushMock,
+    });
+  });
 
   test('renders the specified number of cards', () => {
-    renderWithProviders(<CardList cards={mockCharacters} handleCardClick={mockHandleCardClick} />);
+    renderWithProviders(<CardList cards={mockCharacters} />);
     expect(screen.getByText('Luke Skywalker')).toBeInTheDocument();
     expect(screen.getByText('Darth Vader')).toBeInTheDocument();
     expect(screen.getAllByText(/Skywalker|Vader/)).toHaveLength(2);
   });
 
   test('displays message if no cards are present', () => {
-    renderWithProviders(<CardList cards={[]} handleCardClick={mockHandleCardClick} />);
+    renderWithProviders(<CardList cards={[]} />);
     expect(screen.getByText('No characters found')).toBeInTheDocument();
+  });
+
+  test('navigates to the correct URL when a card is clicked', async () => {
+    renderWithProviders(<CardList cards={mockCharacters} />);
+
+    const lukeSkywalkerCard = screen.getByText('Luke Skywalker');
+    await userEvent.click(lukeSkywalkerCard);
+
+    expect(pushMock).toHaveBeenCalledWith('/?page=1&search=&details=1');
   });
 });
